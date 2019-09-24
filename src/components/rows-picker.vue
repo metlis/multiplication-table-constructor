8bde8b965a4af847fba395e4769ab48f57acd57c
metlis <andrei.rusov8@gmail.com>
2019-09-24 17:41:44 +0300

    rows picker added

---
+++ src/components/rows-picker.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="form-check form-check-inline" v-for="option in rowsOptions" :key="option">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                   :id="option" :value="option" v-model="picked">
+            <label class="form-check-label" :for="option">{{option}}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'rows-picker',
+  data() {
+    return {
+      rowsOptions: [1, 2, 5, 10],
+      picked: '',
+    };
+  },
+  watch: {
+    picked(val) {
+      this.$emit('rowsChange', val);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
